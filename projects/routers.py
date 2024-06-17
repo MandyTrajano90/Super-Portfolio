798578c7508bed2cf7fae6a790d79621991312e4
Mandytrajano90 <mandytrajano90@gmail.com>
2024-06-17 18:37:12 -0300

    Req 3 - Projects CRUD

--- projects/routers.py
+++ projects/routers.py
@@ -1,6 +1,7 @@
 from rest_framework.routers import DefaultRouter
-from .views import ProfileViewSet
+from .views import ProfileViewSet, ProjectViewSet
 
 
 router = DefaultRouter()
 router.register(r'profiles', ProfileViewSet)
+router.register(r'projects', ProjectViewSet)
--- projects/views.py
+++ projects/views.py
@@ -1,9 +1,14 @@
 from rest_framework import viewsets
-from .models import Profile
-from .serializers import ProfileSerializer
+from .models import Profile, Project
+from .serializers import ProfileSerializer, ProjectSerializer
 from django.shortcuts import render
 
 
 class ProfileViewSet(viewsets.ModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
+
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
